5dacc4afac7d2f7d273f4859a6532af282207780
Balaji <balajiadake00@gmail.com>
2026-05-17 11:46:17 +0530

    prime_numbers

---
+++ prime_numbers.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int i;
+
+    for(i = 2; i < n; i++)
+    {
+        if(n % 2== 0)
+        {
+            cout << "Not Prime number";
+            break;
+        }
+    }
+
+         if(i == n)
+        {
+         cout << "Prime Number";
+        }
+
+    return 0;
+
+}
