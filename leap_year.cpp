d77bb980051ca4e03b0639d05a8fbf09ad4b3d30
Balaji <balajiadake00@gmail.com>
2026-05-19 13:06:49 +0530

    leap_year

---
+++ leap_year.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+                
+int main()
+{
+    int year;
+    cout << "Enter Year: ";
+    cin >> year;
+
+    if(year % 400 == 0)
+    {
+        cout << "Leap Year";
+    }
+
+    else if(year % 4 == 0 && year % 100 != 00)
+    {
+        cout << "Leap Year";
+    }
+
+    else
+    {
+        cout << "Not a Leap Year";
+    }
+
+    return 0;
+
+}
--- multiplication.cpp
+++ multiplication.cpp
@@ -11,7 +11,7 @@ int main()
 
     for(int i = 1; i <=10; i++)
     {
-        cout << n * i << endl;
+        cout << n  << " x " <<  i << " = "<< n * i << endl;
     }
 
     return 0;
